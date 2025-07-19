e39f4b54f562313f32d956efd612ed1ebb78cf52
gurrutia15 <gurrutia15@gmail.com>
2025-07-19 15:09:17 -0400

    primeros estilos bootstrap y prueba de modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,15 @@ alert.addEventListener('click', function(e){
     e.preventDefault();
     document.getElementById('successAlert').style.display='block'
 })
+
+// Modal cierre automáticamente luego del tiempo indicado
+document.addEventListener('DOMContentLoaded', function () {
+    const modalElement = document.getElementById('exampleModal');
+    const modal = new bootstrap.Modal(modalElement);
+
+    modalElement.addEventListener('shown.bs.modal', function () {
+      setTimeout(function () {
+        modal.hide();
+      }, 3000); // 3 segundos
+    });
+  });
