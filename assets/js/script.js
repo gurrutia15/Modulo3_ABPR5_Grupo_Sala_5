105a06ae1a68d53653c14d2a2e9606d20bd64a2c
lcarr08 <luiscarrilloq@gmail.com>
2025-07-30 00:04:45 -0400

    iva truncado

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,7 @@ window.renderCarrito = function () {
   });
 
   const neto = carrito.reduce((sum, item) => sum + item.cantidad * item.precio, 0);
-  const iva = neto * 0.19;
+  const iva = Math.trunc(neto * 0.19);
   let bruto = neto + iva;
   const despacho = bruto < 100000 ? bruto * 0.05 : 0;
   bruto += despacho;
