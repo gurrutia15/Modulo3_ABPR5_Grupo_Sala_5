2598f8fd6219333a9d24039a526b8ddcbaf60edb
jlmansilla <juanmancillaster@gmail.com>
2025-07-29 21:17:49 -0400

    cambios en modal

---
+++ assets/js/contacto.js
@@ -0,0 +1,13 @@
+const modalElement = document.getElementById('exampleModal');
+  const modal = new bootstrap.Modal(modalElement);
+  const formulario=document.querySelector('form') 
+  formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario
+  }); 
+  modalElement.addEventListener('shown.bs.modal', function () {
+    debugger;
+    console.log("saludillos aquí")
+    setTimeout(function () {
+      modal.hide();
+    }, 3000); // 3 segundos
+  });
